ec28fb3f11233771c90fb369540db25711f5a224
Pavandkurdekar <pavandkurdekar55@gmail.com>
2025-01-07 13:13:08 +0530

    Get Users

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -4,46 +4,37 @@ import { getUsers } from "../Store/UserSlice";
 
 const Cards = () => {
     const dispatch = useDispatch();
-   // Correctly accessing user state from the store
-   const users = useSelector((state) => state.user.users); // Since the reducer is named 'user' in the store
-   const loading = useSelector((state) => state.user.loading);
-   const error = useSelector((state) => state.user.error);
-    useEffect(() => {
-      console.log("Inside useEffect");
-      dispatch(getUsers()); // Dispatching the async action to fetch users
-    }, [dispatch]);
-  
-    useEffect(() => {
-      console.log("Fetched Users:", users); // Logs the fetched users data
-    }, [users]);
+    const users = useSelector((state) => state.user.users);
+    const loading = useSelector((state) => state.user.loading);
 
-    if(loading)
-    {
-    return(
-        <h2>Loading....</h2>
-    )
+    useEffect(() => {
+        dispatch(getUsers());
+    }, [dispatch]);
+
+    if (loading) {
+        return <h2>Loading....</h2>;
     }
 
-  return (
-    <div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="/" className="card-link">
-            Card link
-          </a>
-          <a href="/" className="card-link">
-            Another link
-          </a>
+    return (
+        <div className="row">
+            {users.map((user) => (
+                <div key={user.id} className="col-md-3 col-sm-6 col-12 mb-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">{user.name}</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">{user.address}</h6>
+                            <p className="card-text">
+                              Gender{user.address}
+                            </p>
+                            <p className="card-text">
+                              Age:{user.id}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Cards;
